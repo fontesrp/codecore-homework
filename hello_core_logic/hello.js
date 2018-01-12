52d8bc9f91af4852313ad083c8807176647cdd87
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-01-12 15:45:41 -0800

    Add file with main global object

---
+++ hello_core_logic/hello.js
@@ -0,0 +1,10 @@
+const hello = {
+    'Tester Board': {
+      'To Do': ['Laundry', 'Buy Apples', 'Pay Phone Bill'],
+      'Doing': ['Laundry', 'Studying Javascript', 'Studying HTML', 'Studying Ruby'],
+      'Done': ['Laundry']
+    },
+    'Dreams': {
+      'Wish List': ['Conquer the Seven Kingdoms', 'Get my baby back', 'My hand needs to chill'],
+    }
+};
