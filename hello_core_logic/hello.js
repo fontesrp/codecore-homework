be17b95722b9840e93a41b1034bbe27dc84cdda4
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-01-12 16:32:08 -0800

    Add function to display a board

--- hello_core_logic/hello.js
+++ hello_core_logic/hello.js
@@ -13,7 +13,7 @@ const listBoards = function () {
 
     const separator = "------------------\n";
 
-    return Object.keys(hello).reduce((str, key, idx) => `${str}${idx + 1}- ${key}\n${separator}`, separator);
+    return Object.keys(hello).reduce((str, name, idx) => `${str}${idx + 1}- ${name}\n${separator}`, separator);
 };
 
 const createBoard = function (boardName) {
@@ -38,8 +38,36 @@ const removeBoard = function (boardName) {
     return `Board ${boardName} was removed`;
 };
 
+const displayBoard = function (boardName) {
+
+    if (hello[boardName] === undefined) {
+        return "Board doesn't exist";
+    }
+
+    const separator = "|--------------\n";
+
+    let str = separator;
+
+    Object.keys(hello[boardName]).forEach(function (list) {
+
+        // Section header
+        str += `| ${list}\n${separator}`;
+
+        // Items
+        const bullet = "|> ";
+        str += bullet + hello[boardName][list].join("\n" + bullet) + "\n" + separator;
+    });
+
+    return str;
+};
+
 console.log("listBoards:\n" + listBoards());
+
 console.log("createBoard('Cuisine'):", createBoard("Cuisine"));
 console.log("createBoard('Tester Board'):", createBoard("Tester Board"));
-console.log("removeBoard('Tester Board'):", removeBoard("Tester Board"))
+
+console.log("removeBoard('Dreams'):", removeBoard("Dreams"))
+
 console.log("listBoards:\n" + listBoards());
+
+console.log("displayBoard('Tester Board'):\n" + displayBoard("Tester Board"))
