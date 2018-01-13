9f9e6c3cabe6de3d6d9a984bd20e2c88c133b160
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-01-12 17:37:37 -0800

    Add all other functions and tests

--- hello_core_logic/hello.js
+++ hello_core_logic/hello.js
@@ -50,10 +50,10 @@ const displayBoard = function (boardName) {
 
     Object.keys(hello[boardName]).forEach(function (list) {
 
-        // Section header
+        // Section header (list name)
         str += `| ${list}\n${separator}`;
 
-        // Items
+        // Items (cards)
         const bullet = "|> ";
         str += bullet + hello[boardName][list].join("\n" + bullet) + "\n" + separator;
     });
@@ -61,13 +61,115 @@ const displayBoard = function (boardName) {
     return str;
 };
 
+const createList = function (boardName, listName) {
+
+    if (hello[boardName] === undefined) {
+        return "Board doesn't exist";
+    }
+
+    if (hello[boardName][listName] !== undefined) {
+        return "List name already exists";
+    }
+
+    hello[boardName][listName] = [];
+
+    // If the list was created, return undefined
+};
+
+const createCard = function (boardName, listName, cardName) {
+
+    if (hello[boardName] === undefined) {
+        return "Board doesn't exist";
+    }
+
+    if (hello[boardName][listName] === undefined) {
+        return "List doesn't exist";
+    }
+
+    hello[boardName][listName].push(cardName);
+
+    // If the card was added, return undefined
+};
+
+const removeList = function (boardName, listName) {
+
+    if (hello[boardName] === undefined) {
+        return "Board doesn't exist";
+    }
+
+    if (hello[boardName][listName] === undefined) {
+        return "List doesn't exist";
+    }
+
+    delete hello[boardName][listName];
+
+    // If the list was deleted, return undefined
+};
+
+const removeCard = function (boardName, listName, cardIdx) {
+
+    if (hello[boardName] === undefined) {
+        return "Board doesn't exist";
+    }
+
+    if (hello[boardName][listName] === undefined) {
+        return "List doesn't exist";
+    }
+
+    if (hello[boardName][listName][cardIdx] === undefined) {
+        return "Card doesn't exist";
+    }
+
+    hello[boardName][listName].splice(cardIdx, 1);
+
+    // If the card was added, return undefined
+};
+
+const moveCard = function (boardName, fromList, toList, fromCardIndex, toCardIndex) {
+
+    if (hello[boardName] === undefined) {
+        return "Board doesn't exist";
+    }
+
+    if (hello[boardName][fromList] === undefined) {
+        return "Source list doesn't exist";
+    }
+
+    if (hello[boardName][fromList][fromCardIndex] === undefined) {
+        return "Card doesn't exist";
+    }
+
+    if (hello[boardName][toList] === undefined) {
+        return "Destination list doesn't exist";
+    }
+
+    if (toCardIndex < 0 || toCardIndex > hello[boardName][toList].length) {
+        return "Invalid destination index";
+    }
+
+    hello[boardName][toList].splice(toCardIndex, 0, hello[boardName][fromList][fromCardIndex]);
+
+    hello[boardName][fromList].splice(fromCardIndex, 1);
+};
+
 console.log("listBoards:\n" + listBoards());
 
-console.log("createBoard('Cuisine'):", createBoard("Cuisine"));
-console.log("createBoard('Tester Board'):", createBoard("Tester Board"));
+console.log("createBoard('Cuisine'): " + createBoard("Cuisine"));
+console.log("createBoard('Tester Board'): " + createBoard("Tester Board"));
 
-console.log("removeBoard('Dreams'):", removeBoard("Dreams"))
+console.log("removeBoard('Dreams'): " + removeBoard("Dreams"))
 
 console.log("listBoards:\n" + listBoards());
 
 console.log("displayBoard('Tester Board'):\n" + displayBoard("Tester Board"))
+
+console.log("createList('Cuisine', 'Types'): " + createList("Cuisine", "Types"));
+console.log("createCard('Cuisine', 'Types', 'Vietnamese'): " + createCard("Cuisine", "Types", "Vietnamese"));
+
+console.log("displayBoard('Cuisine'):\n" + displayBoard("Cuisine"))
+
+console.log("removeList('Tester Board', 'Done'): " + removeList("Tester Board", "Done"));
+console.log("removeCard('Tester Board', 'Doing', 2): " + removeCard("Tester Board", "Doing", 2));
+console.log("moveCard('Tester Board', 'Doing', 'To Do', 1, 1): " + moveCard("Tester Board", "Doing", "To Do", 1, 1))
+
+console.log("displayBoard('Tester Board'):\n" + displayBoard("Tester Board"));
